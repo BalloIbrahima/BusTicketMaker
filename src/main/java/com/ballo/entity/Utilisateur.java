41d99f7f1ff4486c3e1229ad0fd109501f0ac732
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-04-18 23:29:18 +0000

    create class utilisateur

---
+++ src/main/java/com/ballo/entity/Utilisateur.java
@@ -0,0 +1,57 @@
+package com.ballo.entity;
+import javax.persistence.*;
+
+@Entity(name="utilisateur")
+public class Utilisateur {
+	
+	@Id @GeneratedValue
+	private Long id;
+	private String nom;
+	private String prenom;
+	private String pseudo;
+	private String motDePasse;
+	
+	
+	public Utilisateur(String nom, String prenom, String pseudo, String motDePasse) {
+		
+		this.nom = nom;
+		this.prenom = prenom;
+		this.pseudo = pseudo;
+		this.motDePasse = motDePasse;
+	}
+	
+	
+	
+	
+	public Utilisateur() {}
+
+
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+	
+	
+	
+	
+}
