16683f6c78c82f794e4b095177516dbaa58c8a12
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-04-19 00:09:09 +0000

    user dao  partiellement ok

---
+++ src/main/java/com/ballo/dao/UtilisateurDao.java
@@ -0,0 +1,73 @@
+package com.ballo.dao;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.ballo.entity.Utilisateur;
+
+public class UtilisateurDao extends Dao<Utilisateur>{
+
+	@Override
+	public Utilisateur recherche(int id) {
+		// TODO Auto-generated method stub
+		Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Utilisateur user = (Utilisateur) session.get(Utilisateur.class, id);
+        transaction.commit();
+        session.close();
+        return user;
+	}
+
+	@Override
+	public void enregistrer(Utilisateur obj) {
+		// TODO Auto-generated method stub
+		 Session session = HibernateUtil.getSessionFactory().openSession();
+         Transaction transaction = session.beginTransaction();
+         session.save(obj);
+         transaction.commit();
+         session.close();
+		
+	}
+
+	@Override
+	public void modifier(Utilisateur obj, int id) {
+		// TODO Auto-generated method stub
+		Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        Utilisateur user = (Utilisateur) session.load(Utilisateur.class, id);
+        user.setNom(obj.getNom());
+        user.setPrenom(obj.getPrenom());
+        user.setPseudo(obj.getPseudo());
+        
+        session.update(user);
+        transaction.commit();
+        session.close();
+		
+	}
+
+	@Override
+	public void supprimner(Utilisateur obj) {
+		// TODO Auto-generated method stub
+		Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(obj);
+        transaction.commit();
+        session.close();
+		
+	}
+
+	@Override
+	public List<Utilisateur> liste() {
+		// TODO Auto-generated method stub
+		List<Utilisateur> liste = new ArrayList();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Query query = session.createQuery("from Utilisateur");
+        liste = query.list();
+        return liste;
+	}
+
+}
